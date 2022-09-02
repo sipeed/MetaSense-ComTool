9ec1fb2418220731cee6165956ae0c502981c1aa
taorye <taorye@outlook.com>
2022-09-02 18:37:36 +0800

    fix: clean up

--- COMTool/helpAbout.py
+++ COMTool/helpAbout.py
@@ -22,16 +22,14 @@ def HelpInfo():
 {}: {}<br>
 {}: {}<br>
 <div>
+    <div><a style="color:#009688;" href="https://github.com/Neutree/COMTool">COMTool</a>{}</div>
+    <a style="vertical-align: middle;" href="https://neucrack.com">
+        <img src="{}" width=109 height=32/></a>
     <div>{}</div>
     <a style="vertical-align: middle;" href="https://neucrack.com">
         <img src="{}" width=109 height=32/></a>
 </div>
 {}: <b><a style="color:#009688;" href="https://github.com/Neutree/COMTool#license">LGPL-3.0</a></b><br>
-{}<br>
-{} <b><a style="color:#009688;" href="https://github.com/neutree/COMTool">Github</a></b>, {} <b><a style="color:#009688;" href="https://github.com/Neutree/COMTool/releases"> releases {}</a></b><br>
-{} <b><a style="color:#009688;" href="https://github.com/Neutree/COMTool/issues"> issues</a></b><br>
-{}: 566531359 <br><br>
-{}: <a style="color:#009688;" href="https://neucrack.com/donate">neucrack.com/donate<br><img src="{}"/> <img src="{}"/></a><br>
 '''.format(
     parameters.appName,
     version.__version__,
@@ -41,16 +39,9 @@ def HelpInfo():
     parameters.configFilePath,
     _("Old config backup in"),
     os.path.dirname(parameters.configFilePath,),
-    _('COMTool is a Open source project create by'),
+    _(' is a Open source project create by'),
+    '{}/{}'.format(parameters.dataPath, "assets/neucrack.png"),
+    _('and modified by'),
     '{}/{}'.format(parameters.dataPath, parameters.appLogo2),
     _("License"),
-    _('Welcome to improve it together and add plugins'),
-    _('See more details on'),
-    _("and get latest version at"),
-    _("page"),
-    _("Have problem? see"),
-    _("QQ group for plugin development discussion"),
-    _("You can buy me half a cup of coffee if this software helpes you"),
-    os.path.join(parameters.assetsDir, "donate_wechat.jpg"),
-    os.path.join(parameters.assetsDir, "donate_alipay.jpg")
 )
